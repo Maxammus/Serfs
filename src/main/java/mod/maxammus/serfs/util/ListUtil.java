ea860d16626f4dadf0255259c4b31b74b6ea2839
default <default>
2025-02-19 15:37:17 -0500

    add OrDefault methods

--- src/main/java/mod/maxammus/serfs/util/ListUtil.java
+++ src/main/java/mod/maxammus/serfs/util/ListUtil.java
@@ -6,21 +6,43 @@ import java.util.List;
 import java.util.function.Predicate;
 
 public class ListUtil {
-        public static <E> E getOrNull(List<E> list, int index) {
-            if(index >= 0 && list.size() >= index + 1)
-                return list.get(index);
-            return null;
-        }
-        public static <E> E findOrNull(Collection<E> list, Predicate<? super E> predicate) {
-            return list.stream()
-                    .filter(predicate)
-                    .findFirst()
-                    .orElse(null);
-        }
-        public static <E> E findOrNull(E[] array, Predicate<? super E> predicate) {
-            return Arrays.stream(array)
-                    .filter(predicate)
-                    .findFirst()
-                    .orElse(null);
-        }
+    public static <E> E getOrNull(List<E> list, int index) {
+        if(index >= 0 && list.size() >= index + 1)
+            return list.get(index);
+        return null;
+    }
+
+    public static <E> E getOrDefault(List<E> list, int index, E _default) {
+        if(index >= 0 && list.size() >= index + 1)
+            return list.get(index);
+        return _default;
+    }
+
+    public static <E> E findOrNull(Collection<E> list, Predicate<? super E> predicate) {
+        return list.stream()
+                .filter(predicate)
+                .findFirst()
+                .orElse(null);
+    }
+
+    public static <E> E findOrNull(E[] array, Predicate<? super E> predicate) {
+        return Arrays.stream(array)
+                .filter(predicate)
+                .findFirst()
+                .orElse(null);
+    }
+
+    public static <E> E findOrDefault(Collection<E> list, Predicate<? super E> predicate, E _default) {
+        return list.stream()
+                .filter(predicate)
+                .findFirst()
+                .orElse(_default);
+    }
+
+    public static <E> E findOrDefault(E[] array, Predicate<? super E> predicate, E _default) {
+        return Arrays.stream(array)
+                .filter(predicate)
+                .findFirst()
+                .orElse(_default);
+    }
 }
